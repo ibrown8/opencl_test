168a4267bfc0dfb013511a86f4ad5fec331a8b8e
Isaiah <inoel525@gmail.com>
2023-09-21 10:57:48 -0500

    Update

--- src/main.rs
+++ src/main.rs
@@ -24,17 +24,13 @@ fn main() -> Result<()> {
     assert!(0 < platforms.len());
     let platform = &platforms[0];
     let device_ids = platform.get_devices(CL_DEVICE_TYPE_GPU)?;
-    println!("CL_DEVICE_TYPE_GPU count: {}", device_ids.len());
     assert!(0 < device_ids.len());
     let dev_id = device_ids[0];
     let device = Device::new(dev_id);
-    println!("Device Created");
     let context = Context::from_device(&device)?;
-    println!("Context Created");
     let command_queue = unsafe {
         CommandQueue::create_with_properties(&context, dev_id, 0 as u64, 0)?
     };
-    println!("Command Queue Created");
     let read_flags = CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR;
     let write_flags = CL_MEM_WRITE_ONLY | CL_MEM_USE_HOST_PTR;
     let mut vec_a : Vec<i32> = (0..1024).collect();
@@ -49,36 +45,25 @@ fn main() -> Result<()> {
     let mut buff_c : Buffer<i32> = unsafe {
         Buffer::create(&context, write_flags, 1024, vec_c.as_mut_ptr() as *mut c_void)?
     };
-    println!("Buffers Created");
     let program = Program::create_and_build_from_source(&context, MY_PROGRAM, "").unwrap();
-    println!("Program Created");
     let build_status = program.get_build_status(dev_id)?;
-    println!("build_status : {}", build_status);
     let log = program.get_build_log(dev_id)?;
-    println!("log : {}", log);
-    println!("num kernels : {}", program.get_num_kernels()?);
     let kernel = Kernel::create(&program, "vadd")?;
     let global_size : usize = 1024;
-    println!("Kernel Created");
     unsafe {
-        println!("type of arg 0 : {:?}", kernel.get_arg_type_name(0)?);
-        println!("{:?}", kernel.set_arg(0, &buff_a)?);
-        println!("{:?}", kernel.set_arg(1, &buff_b)?);
-        println!("{:?}", kernel.set_arg(2, &buff_c)?);
-        println!("{:?}", kernel.set_arg(3, &(1024 as u32))?);
+        kernel.set_arg(0, &buff_a)?;
+        kernel.set_arg(1, &buff_b)?;
+        kernel.set_arg(2, &buff_c)?;
+        kernel.set_arg(3, &(1024 as u32))?;
     }
-    println!("Arguments Set");
     let work_group_size : usize = 64;
-    println!("Work group size : {}", work_group_size);
-    println!("Function name : {}", kernel.function_name()?);
-    println!("Number of args : {}", kernel.num_args()?);
-    println!("Attributes : {}", kernel.attributes()?);
     let global_offset : usize = 0;
     let event = unsafe {
-        command_queue.enqueue_nd_range_kernel(kernel.into(), 1, &global_offset, &global_size, &work_group_size, &[])?
+        command_queue.enqueue_nd_range_kernel(kernel.get(), 1, &global_offset, &global_size, &work_group_size, &[]).unwrap()
     };
+    println!("Kernel run");
     let event2 = unsafe {
-        command_queue.enqueue_read_buffer(&buff_c, opencl3::memory::CL_TRUE, 0, vec_c.as_mut_slice(), &[cl_event::from(event)])?
+        command_queue.enqueue_read_buffer(&buff_c, opencl3::memory::CL_TRUE, 0, vec_c.as_mut_slice(), &[event.get()])?
     };
     for i in &vec_c {
         if *i > 16 {
